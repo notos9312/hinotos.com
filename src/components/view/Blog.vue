a70c8606f78436ad2c14bb00dcd6b38850d0fb5d
Notos <lianggh159@outlook.com>
2018-04-02 22:05:05 +0800

    reverse and hide the first two blogs

--- src/components/view/Blog.vue
+++ src/components/view/Blog.vue
@@ -21,7 +21,11 @@ export default {
   },
   data() {
     return {
-      titleData: []
+      titleData: [],
+      filterTitleArray:[
+        "5a5108ef3d0471bbf05c8827", //MongoDB安装及配置
+        "5a787bc19be7073c15e34e06", //挂载数据盘到Ubuntu服务器
+      ],
     }
   },
   mounted: function(){
@@ -36,11 +40,14 @@ export default {
         { emulateJSON: true }
       ).then(
         res => {
-            this.titleData = res.body;
+            this.titleData = res.body.sort((i,j)=>{return j.createTime - i.createTime}).filter(this.filterTitleObjFunc);
         },
         err => {
           console.log(err.status);
       });
+    },
+    filterTitleObjFunc: function(titleObj){
+      return (this.filterTitleArray.indexOf(titleObj.objectId) == -1);
     }
   }
 }
